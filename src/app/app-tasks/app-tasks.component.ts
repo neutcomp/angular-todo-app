13602dc96f43b866d8e37ff33c8b1e8cfec224db
Bjorn van der Neut <bjornvdneut+github@gmail.com>
2025-02-07 15:22:35 +0100

    First push of Todo App

---
+++ src/app/app-tasks/app-tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-tasks',
+  imports: [],
+  templateUrl: './app-tasks.component.html',
+  styleUrl: './app-tasks.component.css'
+})
+export class AppTasksComponent {
+
+}
---
+++ src/app/users.ts
@@ -0,0 +1,22 @@
+export const DUMMY_USERS = [
+    {
+      id: 'u1',
+      name: 'Bjorn',
+      avatar: 'bjorn.png',
+    },
+    {
+      id: 'u2',
+      name: 'Lea',
+      avatar: 'lea.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Ruben',
+      avatar: 'ruben.jpg',
+    },
+    {
+      id: 'u4',
+      name: 'Sara',
+      avatar: 'sara.jpg',
+    }
+]
